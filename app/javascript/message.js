4f737c1692201c17c3469dfeeab06423f8db5754
Yutaka8842 <inoue.8842@gmail.com>
2021-06-08 20:45:53 +0900

    .

--- app/javascript/message.js
+++
@@ -1,9 +0,0 @@
-const message = () => {
-  if ( document.getElementById('scroll-message')){
-let target = document.getElementById('scroll-message');
-target.scrollTop = target.scrollHeight;
-
-  }
-}
-
-window.addEventListener("load", message);
